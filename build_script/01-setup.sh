ecb02516841fb02d70346d56c202891c84ef4151
huangguiniab <47934913+huangguiniab@users.noreply.github.com>
2021-11-17 23:27:44 +0800

    First commit.

---
+++ build_script/01-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+	apt update
+	apt install gnupg wget apt-transport-https libx11-dev libxxf86vm-dev libxt-dev libxcursor-dev libxrandr-dev -y
+	wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | apt-key add -
+	echo "deb https://adoptopenjdk.jfrog.io/adoptopenjdk/deb buster main"  > /etc/apt/sources.list.d/Adoptopenjdk.list
+	apt update
+	apt install adoptopenjdk-8-hotspot ant gcc g++ -y
+	echo All set OK !
---
+++ build_script/02-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /lwjgl
+rm platform_build/linux_ant/build.xml
+cp /build.xml platform_build/linux_ant/build.xml
+ant generate-all
+ant compile
+ant compile_native
+ant jars
+cp libs/linux/liblwjgl.so dist/liblwjgl.so
+cp libs/linux/liblwjgl.so dist/liblwjgl64.so
+cp libs/lwjgl* dist/
+cd dist/
+jar cvf lwjgl_platform.jar liblwjgl.so liblwjgl64.so
