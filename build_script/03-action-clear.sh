c3fe0f29457dbc153ef1d49edf2f16ba50645e52
huangguiniab <47934913+huangguiniab@users.noreply.github.com>
2021-11-18 09:18:59 +0800

    第一个正式发行版本 添加了对于封装成jar的本地库的支持

--- build_script/03-action-clear.sh
+++ build_script/03-action-clear.sh
@@ -14,13 +14,17 @@ sudo cp arm64/lwjgl/dist/lwjgl_util_applet.jar dist/
 #arm64
 sudo mv arm64/lwjgl/dist/liblwjgl64.so arm64/lwjgl/dist/liblwjgl_arm64.so
 sudo mv arm64/lwjgl/dist/libopenal.so arm64/lwjgl/dist/libopenal_arm64.so
+sudo mv arm64/lwjgl/dist/lwjgl_platform.jar arm64/lwjgl/dist/lwjgl_platform_arm64.jar
 
 sudo cp arm64/lwjgl/dist/liblwjgl_arm64.so dist/
 sudo cp arm64/lwjgl/dist/libopenal_arm64.so dist/
+sudo cp arm64/lwjgl/dist/lwjgl_platform_arm64.jar /dist
 
 #armhf
 sudo mv armhf/lwjgl/dist/liblwjgl.so armhf/lwjgl/dist/liblwjgl_armhf.so
 sudo mv armhf/lwjgl/dist/libopenal.so armhf/lwjgl/dist/libopenal_armhf.so
+sudo mv armhf/lwjgl/dist/lwjgl_platform.jar armhf/lwjgl/dist/lwjgl_platform_armhf.jar
 
 sudo cp armhf/lwjgl/dist/liblwjgl_armhf.so dist/
 sudo cp armhf/lwjgl/dist/libopenal_armhf.so dist/
+sudo cp armhf/lwjgl/dist/lwjgl_platform_armhf.jar dist/
